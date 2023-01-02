da5b1397dc25875f86ff2abaa959cf82bfca0af8
NoLifeKing <webmaster@itssimple.se>
2023-01-02 08:04:31 +0100

    aaand reset settings

--- CFLookup/Program.cs
+++ CFLookup/Program.cs
@@ -49,12 +49,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts();
+    app.UseHsts();
 }
 
 app.UseForwardedHeaders();
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseStaticFiles(new StaticFileOptions
 {
     OnPrepareResponse = ctx =>
