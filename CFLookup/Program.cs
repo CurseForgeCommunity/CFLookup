ef4e5584aff10c13cfd92517a4a24a7ddc123e6f
Chris Gårdenberg <chga@multinet.se>
2023-07-07 11:16:40 +0200

    feat: New API controller for grabbing the stats available
    New URL endpoints are:
    - `/api/Stats/Minecraft/ModStats.json`
    - `/api/Stats/Minecraft/ModpackStats.json`
    
    fixes #12

--- CFLookup/Program.cs
+++ CFLookup/Program.cs
@@ -30,11 +30,31 @@ builder.Services.AddRazorPages();
 
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton(ConnectionMultiplexer.Connect("127.0.0.1:6379"));
+var cfApiKey = string.Empty;
+var redisServer = string.Empty;
+
+if (OperatingSystem.IsWindows())
+{
+    cfApiKey = Environment.GetEnvironmentVariable("CFAPI_Key", EnvironmentVariableTarget.Machine) ??
+        Environment.GetEnvironmentVariable("CFAPI_Key", EnvironmentVariableTarget.User) ??
+        Environment.GetEnvironmentVariable("CFAPI_Key", EnvironmentVariableTarget.Process) ??
+        string.Empty;
+
+    redisServer = Environment.GetEnvironmentVariable("RedisServer", EnvironmentVariableTarget.Machine) ??
+        Environment.GetEnvironmentVariable("RedisServer", EnvironmentVariableTarget.User) ??
+        Environment.GetEnvironmentVariable("RedisServer", EnvironmentVariableTarget.Process) ??
+        "127.0.0.1:6379";
+}
+else
+{
+    cfApiKey = Environment.GetEnvironmentVariable("CFAPI_Key") ?? string.Empty;
+    redisServer = Environment.GetEnvironmentVariable("RedisServer") ?? "127.0.0.1:6379";
+}
+
+builder.Services.AddSingleton(ConnectionMultiplexer.Connect(redisServer));
 
 builder.Services.AddScoped(options =>
 {
-    var cfApiKey = Environment.GetEnvironmentVariable("CFAPI_Key");
     return new CurseForge.APIClient.ApiClient(cfApiKey, 201, "whatcfprojectisthat@nolifeking85.tv");
 });
 
@@ -47,7 +67,7 @@ app.UseResponseCaching();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    _ = app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     //app.UseHsts();
 }
@@ -59,7 +79,7 @@ app.UseStaticFiles(new StaticFileOptions
 {
     OnPrepareResponse = ctx =>
     {
-        ctx.Context.Response.Headers.Append("Cache-Control", $"public, max-age={(60 * 60 * 24 * 30)}");
+        ctx.Context.Response.Headers.Append("Cache-Control", $"public, max-age={60 * 60 * 24 * 30}");
     }
 });
 
---
+++ CFLookup/StatsController.cs
@@ -0,0 +1,65 @@
+using CurseForge.APIClient;
+using CurseForge.APIClient.Models.Mods;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+
+namespace CFLookup
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatsController : ControllerBase
+    {
+        private readonly ApiClient _cfApiClient;
+        private readonly IDatabaseAsync _redis;
+
+        public ConcurrentDictionary<string, ConcurrentDictionary<ModLoaderType, long>> MinecraftStats = new();
+        public TimeSpan? CacheExpiration { get; set; }
+
+        public StatsController(ApiClient cfApiClient, ConnectionMultiplexer connectionMultiplexer)
+        {
+            _cfApiClient = cfApiClient;
+            _redis = connectionMultiplexer.GetDatabase(5);
+        }
+
+        [HttpGet("Minecraft/ModStats.json")]
+        public async Task<IActionResult> MinecraftModStats()
+        {
+            var minecraftStats = (await SharedMethods.GetMinecraftModStatistics(_redis, _cfApiClient))
+                .OrderBy(gvt => Regex.Replace(gvt.Key, "\\d+", m => m.Value.PadLeft(10, '0')));
+            var cacheExpiration = await _redis.KeyTimeToLiveAsync("cf-mcmod-stats");
+
+            return new JsonResult(new
+            {
+                Stats = minecraftStats,
+                CacheExpiration = GetTruncatedTime(cacheExpiration.Value)
+            });
+        }
+
+        [HttpGet("Minecraft/ModpackStats.json")]
+        public async Task<IActionResult> MinecraftModpackStats()
+        {
+            var minecraftStats = (await SharedMethods.GetMinecraftModpackStatistics(_redis, _cfApiClient))
+                .OrderBy(gvt => Regex.Replace(gvt.Key, "\\d+", m => m.Value.PadLeft(10, '0')));
+            var cacheExpiration = await _redis.KeyTimeToLiveAsync("cf-mcmodpack-stats");
+
+            return new JsonResult(new
+            {
+                Stats = minecraftStats,
+                CacheExpiration = GetTruncatedTime(cacheExpiration.Value)
+            });
+        }
+
+        private static DateTimeOffset GetTruncatedTime(TimeSpan timeSpan)
+        {
+            // return a DateTimeOffset without ticks or milliseconds to get a static value
+            var now = DateTimeOffset.UtcNow;
+            now = now.Add(timeSpan);
+
+            now = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second, now.Offset);
+
+            return now;
+        }
+    }
+}
