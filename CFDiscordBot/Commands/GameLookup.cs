0edb3ef5ef12e8d43894f883fb0d72ad25425b4c
Chris Gårdenberg <chga@multinet.se>
2024-02-02 09:20:52 +0100

    fix: Handle missing game

--- CFDiscordBot/Commands/GameLookup.cs
+++ CFDiscordBot/Commands/GameLookup.cs
@@ -11,9 +11,18 @@ namespace CFDiscordBot.Commands
             int gameId
         )
         {
-            var game = await apiClient.GetGameAsync(gameId);
+            CurseForge.APIClient.Models.Games.Game gameInfo = null;
+            try
+            {
+                var game = await apiClient.GetGameAsync(gameId);
+                gameInfo = game.Data;
+            }
+            catch
+            {
+                // Empty because, ugh
+            }
 
-            if (game is null)
+            if (gameInfo is null)
             {
                 await RespondAsync($"Game with id {gameId} was not found.", ephemeral: true);
 
@@ -25,19 +34,19 @@ namespace CFDiscordBot.Commands
             var mods = await apiClient.SearchModsAsync(gameId, pageSize: 1);
 
             var embed = new EmbedBuilder()
-                .WithTitle(game.Data.Name)
+                .WithTitle(gameInfo.Name)
                 .WithDescription($"This game has {mods.Pagination.TotalCount:n0} mods available on CurseForge.")
-                .WithUrl($"https://www.curseforge.com/{game.Data.Slug}")
+                .WithUrl($"https://www.curseforge.com/{gameInfo.Slug}")
                 .WithColor(Color.DarkOrange);
 
-            if (!string.IsNullOrWhiteSpace(game.Data.Assets.IconUrl))
+            if (!string.IsNullOrWhiteSpace(gameInfo.Assets.IconUrl))
             {
-                embed.WithThumbnailUrl(game.Data.Assets.IconUrl);
+                embed.WithThumbnailUrl(gameInfo.Assets.IconUrl);
             }
 
-            if (!string.IsNullOrWhiteSpace(game.Data.Assets.CoverUrl))
+            if (!string.IsNullOrWhiteSpace(gameInfo.Assets.CoverUrl))
             {
-                embed.WithImageUrl(game.Data.Assets.CoverUrl);
+                embed.WithImageUrl(gameInfo.Assets.CoverUrl);
             }
 
             await RespondAsync(embeds: new[] { embed.Build() }, ephemeral: true);
