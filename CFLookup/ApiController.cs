2fb7b4193c9634729313807b211695e0d3d1bc8a
NoLifeKing <github-things@nolifeking85.tv>
2025-04-21 13:05:59 +0200

    fix: Force int in path to allow multiple methods

--- CFLookup/ApiController.cs
+++ CFLookup/ApiController.cs
@@ -42,7 +42,7 @@ namespace CFLookup
             }
         }
 
-        [HttpGet("/{projectId}.json")]
+        [HttpGet("/{projectId:int}.json")]
         public async Task<IActionResult> GetProject(int projectId)
         {
             try
@@ -62,7 +62,7 @@ namespace CFLookup
             }
         }
 
-        [HttpGet("/file-{fileId}.json")]
+        [HttpGet("/file-{fileId:int}.json")]
         public async Task<IActionResult> GetFile(int fileId)
         {
             try
